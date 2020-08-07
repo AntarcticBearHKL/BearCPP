755244c06a38364bc68bce7d753ca5521165c3c8
a <a@a.com>
2020-08-08 06:19:18 +0800

    'a'

--- FileRead.cpp
+++ FileRead.cpp
@@ -5,7 +5,7 @@
 #include <boost/filesystem/fstream.hpp>
 
 int main(int argc, char** argv){
-	boost::filesystem::path p("E:\\tushare\\000003.SZ");
-	std::cout << p;
-	boost::filesystem::fstream::open(p);
+	boost::filesystem::path p("F:\\Github/FileBear/Python/Windows/PlaygroundAlpha.bat");
+	boost::filesystem::file_status s = boost::filesystem::status(p);
+	std::cout << boost::filesystem::is_regular_file(s) << std::endl;
 }
